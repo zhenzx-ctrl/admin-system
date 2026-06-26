e8304489432bfa6b51c4570434d29a14a44e3bd3
zhenzx-ctrl <3068296362@qq.com>
2026-06-26 13:58:52 +0800

    使用最简配置

--- vite.config.js
+++ vite.config.js
@@ -5,25 +5,6 @@ import path from 'path'
 export default defineConfig({
   plugins: [vue()],
   base: './',
-  build: {
-    target: 'esnext',
-    minify: 'esbuild',
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            if (id.includes('element-plus')) {
-              return 'element-plus'
-            }
-            if (id.includes('echarts')) {
-              return 'echarts'
-            }
-            return 'vendor'
-          }
-        }
-      }
-    }
-  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
